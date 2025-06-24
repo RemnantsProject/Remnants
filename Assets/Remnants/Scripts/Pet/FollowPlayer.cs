52f7172c133da652448c2ddb9d0e2582f1296129
shinhs626 <ahddud89@gmail.com>
2025-06-24 15:37:40 +0900

    1.펫이 플레이어 따라가는 코드 구현(NavMeshAgent)

---
+++ Assets/Remnants/Scripts/Pet/FollowPlayer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Remnants
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        #region Variables
+        //플레이어를 따라가게 하기 위한 NavMeshAgent
+        private NavMeshAgent navMeshAgent;
+        private GameObject target;
+        #endregion
+
+        #region Unity Event Method
+        private void Start()
+        {
+            //참조
+            navMeshAgent = this.GetComponent<NavMeshAgent>();
+        }
+        private void Update()
+        {
+            FollowToPlayer();
+        }
+        #endregion
+
+        #region Custom Method
+        //태그가 Player인 오브젝트를 따라가는 코드
+        private void FollowToPlayer()
+        {
+            target = GameObject.Find("Player");
+
+            //target(플레이어)가 null이 아니라면
+            if (target != null)
+                navMeshAgent.SetDestination(target.transform.position);
+        }
+        #endregion
+    }
+}
+
